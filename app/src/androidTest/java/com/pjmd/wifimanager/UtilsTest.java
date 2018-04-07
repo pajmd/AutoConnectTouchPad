9b5107ab362526670cf6bfc33258db40961b49a2
pjmd <philippe@t-jmd.co.uk>
2018-04-07 13:39:32 +0100

    adding some comments, applied recommendation

---
+++ app/src/androidTest/java/com/pjmd/wifimanager/UtilsTest.java
@@ -0,0 +1,15 @@
+package com.pjmd.wifimanager;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by pjmd on 07/04/18.
+ */
+public class UtilsTest {
+    @Test
+    public void reconectWifi() throws Exception {
+    }
+
+}
